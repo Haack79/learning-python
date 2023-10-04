1224a975e85adaad6ab315005152aa9256115d5a
Brian Haack <haack79@hotmail.com>
2023-10-04 00:40:02 -0700

    updated car class

--- car.py
+++ car.py
@@ -6,6 +6,12 @@ class Car:
         self.model = model
         self.year = year
 
+    @classmethod
+    def get_number_of_wheels(
+        cls,
+    ):  # cls is the class itself and not the instance of the class like self and is used to access class attributes and methods and shared amongst all instances of the class
+        return 4
+
     def get_descriptive_name(self):
         long_name = str(self.year) + " " + self.make + " " + self.model
         return long_name.title()

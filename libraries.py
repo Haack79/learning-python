98b8e774e46bf228614b47fdfac7e3d0dc9d37e0
Brian Haack <haack79@hotmail.com>
2023-10-10 22:43:08 -0700

    Oct10th updated libraries info

---
+++ libraries.py
@@ -0,0 +1,12 @@
+# pypi awesome service for python libraries https://pypi.org/ helps you install 3rd party libraries and packages for python
+# pip install <library name> to install a library
+# pip freeze to see all the libraries you have installed
+# os library is used to interact with the operating system
+# os.getcwd() returns the current working directory
+# os.chdir() changes the current working directory
+# os.listdir() returns a list of all the files in the current working directory
+# python -m pip install --upgrade pip to upgrade pip or some package to ensure you have the latest version
+# tests are used to ensure your code is working as expected
+# unittest is a python library that helps you write tests
+# unittest.TestCase is the class you inherit from to write tests
+# make sure to name your test functions with test_ at the beginning
